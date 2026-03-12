a99d5983b17211a95bbb2cd3e8ff2f7b6e039749
Sarthak Sharma <sarthaknatu001@gmail.com>
2026-03-12 23:05:15 +0530

    eda

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+import sys
+
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(LOG_PATH, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_PATH, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+    
+    

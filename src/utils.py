59cca16bce55d906fe67106ee18544bc186afb44
Sarthak Sharma <sarthaknatu001@gmail.com>
2026-03-15 21:28:51 +0530

    deployment configration

--- src/utils.py
+++ src/utils.py
@@ -23,23 +23,38 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models, params=None):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            if params and model_name in params:
-                grid_search = GridSearchCV(estimator=model, param_grid=params[model_name], cv=5, n_jobs=-1)
-                grid_search.fit(X_train, y_train)
-                model = grid_search.best_estimator_
-            else:
-                model.fit(X_train, y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            #model.fit(X_train, y_train)  # Train model
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)

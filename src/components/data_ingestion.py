e30ab653a9bff8d8cc61de299c7c9746b9bb7c05
Sarthak Sharma <sarthaknatu001@gmail.com>
2026-03-15 08:10:35 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from data_transformation import DataTransformation
-from data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
 
 
 @dataclass
@@ -46,6 +50,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data, test_data, raw_data = obj.initiate_data_ingestion()
+    train_data,test_data,_= obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    
--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,24 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params=None):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            if params and model_name in params:
+                grid_search = GridSearchCV(estimator=model, param_grid=params[model_name], cv=5, n_jobs=-1)
+                grid_search.fit(X_train, y_train)
+                model = grid_search.best_estimator_
+            else:
+                model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
